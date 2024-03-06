ef00262a1750f06a71c686466952d6f753ee7b0d
Lorenzo Bonincontro <116840033+BonincontroL@users.noreply.github.com>
2024-03-06 21:53:05 +0100

    resetta.sh

--- SO_project/resetta.sh
+++ SO_project/resetta.sh
@@ -13,7 +13,4 @@ ipcs -m | grep '^0x' | awk '{print "-m " $2}' | xargs ipcrm
 # Rimuovere tutti i semafori
 ipcs -s | awk '{if ($1 ~ /^[0-9]/) print "-s " $2}' | xargs ipcrm
 
-# Rimuovere tutti i semafori POSIX
-ipcs -s -p | awk '{if ($1 ~ /^[0-9]/) print $1, $2}' | xargs -n 2 ipcrm
-
 echo "Tutte le risorse IPC rimosse con successo."
